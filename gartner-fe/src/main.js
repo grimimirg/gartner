94eb2a45acc59f77ae7e6049f66f83289d4bfea5
Andrea Grimandi <andrea.grimandi1@protonmail.ch>
2023-09-16 13:24:47 +0200

    refactoring - added logger, refactoring of store modules and added new constants

--- gartner-fe/src/main.js
+++ gartner-fe/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+
+import VueLogger from "vuejs3-logger";
+
 import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,5 +10,16 @@ import "bootstrap-icons/font/bootstrap-icons.min.css";
 
 const app = createApp(App);
 const pinia = createPinia();
+
+// store
 app.use(pinia);
+
+// logger
+app.use(VueLogger, {
+  isEnabled: true,
+  stringifyArguments: true,
+  separator: "-",
+  showConsoleColors: true,
+});
+
 app.mount("#app");
--- gartner-fe/src/stores/constants.js
+++ gartner-fe/src/stores/constants.js
@@ -3,5 +3,10 @@ import { defineStore } from "pinia";
 export const useConstantStore = defineStore("consants", {
   state: () => ({
     baseURL: "http://localhost:5000",
+    env: {
+      ...PRODUCTION,
+      ...TEST,
+      ...DEVELOPMENT,
+    },
   }),
 });
